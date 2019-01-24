8621c40e7d57fa623e8533c5e3cb319449f4b4d9
Pcnitp <44447862+Pcnitp@users.noreply.github.com>
2019-01-24 12:56:58 +0530

    Add files via upload

--- word_dictionary.py
+++ word_dictionary.py
@@ -3,25 +3,46 @@ from nltk.tokenize import word_tokenize
 
 stop_words = set(stopwords.words('english'))
 
-file=open("C:\\Users\\DELL\\Desktop\\Project\\New data\\test_extended.txt","r")
-word_dict={}
-while (True):
-    key=file.readline()
-    if not key:
-        break
-    word=word_tokenize(key)
-    if word[0] in stop_words:
-        continue
-    if key in word_dict:
-        word_dict[key]+=1
-    else:
-        word_dict[key]=1
-        
-#sorting in reverse        
-word_dict1=sorted(word_dict.items(), key = lambda kv:(kv[1], kv[0]),reverse=True) 
-
-#print (word_dict)        
-file1=open("C:\\Users\\DELL\\Desktop\\Project\\New data\\word_dict.txt","w")
-for obj in word_dict1:
-    file1.write(obj[0]+str(obj[1])+"\n")
+def createDict(query,SE):
+    file=open("C:\\Users\\DELL\\Desktop\\Project\\"+SE+"\\paraExtended\\"+query+".txt","r",encoding="utf-8")
+    word_dict={}
+    while (True):
+        key=file.readline()
+        if not key:
+            break
+        word=word_tokenize(key)
+        if word[0] in stop_words:
+            continue
+        if key in word_dict:
+            word_dict[key]+=1
+        else:
+            word_dict[key]=1
+            
+    #sorting in reverse        
+    word_dict1=sorted(word_dict.items(), key = lambda kv:(kv[1], kv[0]),reverse=True) 
     
+    #print (word_dict)        
+    file1=open("C:\\Users\\DELL\\Desktop\\Project\\"+SE+"\\word_dict\\"+query+".txt","w",encoding="utf-8")
+    for obj in word_dict1:
+        file1.write(obj[0]+str(obj[1])+"\n")
+    
+
+#createDict("swine flu vaccine","Bing")
+
+file=open("C:\\Users\\DELL\\Desktop\\Project\\query_lists.txt",'r')
+while(True):
+    query=file.readline()
+    if(query==""):
+        break
+    l=len(query)
+    query=query[:l-1]
+    
+    #address="C:\\Users\\DELL\\Desktop\\Project\\Google\\QueryLinks\\"+query+".txt"
+    #parseLink(query, address, "Google")
+    
+    #address="C:\\Users\\DELL\\Desktop\\Project\\Bing\\QueryLinks\\"+query+".txt"
+    createDict(query, "Bing")
+    
+
+
+
