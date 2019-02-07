bbf0b174c37997087c99ce8340b9caa1c773c278
Pcnitp <44447862+Pcnitp@users.noreply.github.com>
2019-02-07 12:08:15 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,7 +1,26 @@
 import requests
-import bs4
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 
+
+def el_is_visible(element):
+    try:
+        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+            return False
+        if isinstance(element, Comment):
+            return False
+        return True
+    except:
+        return False
+
+def get_visible_text(soup):
+    try:
+        text = soup.findAll(text=True)
+        text = ' '.join([t.strip() for t in text if el_is_visible(t)])
+        return text
+    except:
+        return ''
+  
+    
 def parseLink(query, adress, SE):
     file=open(adress,'r')
     while(True):
@@ -19,30 +38,32 @@ def parseLink(query, adress, SE):
             soup = BeautifulSoup(page.text,'html.parser')
         except:
             continue
- 
+        
+        text=get_visible_text(soup)
         #writing content of each page to a text file
-        with open("C:\\Users\\DELL\\Desktop\\Project\\"+SE+"\\paragraph\\"+query+".txt", 'a',encoding="utf-8") as file1:
-            for x in soup.find_all('div'):
+        with open("C:\\Users\\DELL\\Desktop\\Project\\"+SE+"\\paragraph1\\"+query+".txt", 'a',encoding="utf-8") as file1:
+            '''for x in soup.find_all('div'):
                 file1.write(x.get_text()+"\n")
-            
+            for x in soup.find_all('p'):
+                file1.write(x.get_text()+"\n")
+                '''
+            file1.write(text)
 
-              
+            
 file=open("C:\\Users\\DELL\\Desktop\\Project\\query_lists.txt",'r')
 #i=0
 while(True):
     query=file.readline()
-    
     if(query==""):
         break
     l=len(query)
     query=query[:l-1]
     
-    address="C:\\Users\\DELL\\Desktop\\Project\\Google\\QueryLinks\\"+query+".txt"
-    parseLink(query, address, "Google")
+    #address="C:\\Users\\DELL\\Desktop\\Project\\Google\\QueryLinks\\"+query+".txt"
+    #parseLink(query, address, "Google")
     
     address="C:\\Users\\DELL\\Desktop\\Project\\Bing\\QueryLinks\\"+query+".txt"
     parseLink(query, address, "Bing")
-    
     
     
     
